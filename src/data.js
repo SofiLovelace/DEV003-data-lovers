bf221d43866a30fc206abff520319df869db677a
SofiLovelace <kaozztrs@gmail.com>
2023-01-09 07:22:46 -0600

    Agrego a data.js funciones searchData y filterData

--- src/data.js
+++ src/data.js
@@ -1,9 +1,33 @@
-// estas funciones son de ejemplo
+const dataManagement = {
+  
+  /************ Comenzamos con una funcion de busqueda por nombre, si el usuario no ha ingresado nombre se muestran todos */
+  searchData:function(data, searchedName){
+    const dataSought = [];
+    //let dataImgUrl
+    Object.entries(data).forEach (function (cadaArray) {  //usamos forEach para iterar cada arreglo, buscamos en cada arreglo los datos que coincidan con la busqueda
+      const clave = cadaArray[0].toUpperCase(); //convertimos a mayusculas para no tener problema
+      const valor = cadaArray[1];
+      if (clave.indexOf(searchedName, 0) === 0){ // si el indece es diferente a -1, ese es nuestra data buscada
+      //if (clave.includes(inputUser, )) {  // si dentro del string esta lo buscado devuelve true
+        dataSought.push(valor);  // concatenamos dentro del array todos los valores que puedan coincidir 
+      }
+    } ) 
+    return dataSought ; //devolvemos la data con los nombres encontrados
+    /*****Object.entries*****//****con filter****/
+    /*************Averuguar por que filer no funciona con indexOf********
+    const dataEntries = Object.entries(data).filter(function searchUser (cadaArray) {
+      return cadaArray 
+    })
+    console.log(dataEntries); */
+  },
+  filterData:function(data, type) {
+    const arrayFiltredOut = data.filter(function(element){
+      return element.tags[0] === type || element.tags[1] === type;
+    })
+    return arrayFiltredOut;
+  }
+}
+export default dataManagement;
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
+/*****Object.entries*****//****con forEach****/
+//console.log(Object.entries(data.data)); // Devuelve un array de arrays, con propiedad y valor en cada array
--- src/main.js
+++ src/main.js
@@ -1,3 +1,21 @@
-import { example } from './data.js';
+import dataManagement from './data.js';
 import data from './data/lol/lol.js';
-console.log(example, data.data.Aatrox.tags);
+const firstDataSet = JSON.parse(JSON.stringify(data.data));  /// creamos una primer copia profunda con un metodo de serialización, convertimos a array convertimos a objeto de vuelta pra romper la referencia
+//const inputUser = prompt("Ingresa el nombre del campeon").toUpperCase(); //convertimos a mayusculas para no tener problemas
+
+////////////////////////////////////////
+/**********ESTUDIANDO OBJETOS*********/
+////////////////////////////////////////
+// /*****Object.keys******/
+// console.log(Object.keys(data.data)); // Obtenemos en un array todos las propiedades que contiene el objeto (si  alguna de las propiedades es un valor distinto a string tener cuidado ya que el array se desordena)
+// console.log(Object.keys(data.data).length); // Al array obtenido le podemos aplicar el metodo .length y asi podemos saber cuantas propiedades tiene el objeto, incluso podemos iterar con for o mas metodos para arrays
+// /*****Object.keys******/
+// console.log(Object.values(data.data)); // Devuelve un array con los valores de cada propiedad del objeto, no devuelve los nombres de las propiedades. Una ventaja es que no hay parseo de datos a string, concatena en el array sin modificar el valor de cada elemento
+
+
+/*Pasamos parametro para search data y almacenamos los valores encontrado en una constante */
+const  searchedName = dataManagement.searchData(firstDataSet, "");
+console.log(searchedName);
+//pasamos parametros para filterData y almacenamos en variable
+const filteredOut = dataManagement.filterData(searchedName, "Fighter");
+console.log(filteredOut);
